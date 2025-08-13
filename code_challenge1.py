c52668b8ac78a42f48046a8748593b4187a456eb
Reyes, Jomar S. <jomar.sumilang90038@gmail.com>
2025-08-13 12:18:08 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,3 @@
+name = input("type your name --> ")
+x = ("Hello")
+print("\t\t\t\t\t\t*\n\t\t\t\t\t*\t\t*\n\t\t\t\t*\t\t\t\t*\n\t\t\t*\t ",x," \t\t\t\t*\n\t\t*\t\t\t\t ",name," \t\t\t*\n\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t*")
