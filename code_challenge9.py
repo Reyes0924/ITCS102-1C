41b411c95a3978843bc08bacedabe2d358dd75c1
Reyes, Jomar S. <jomar.sumilang90038@gmail.com>
2025-09-20 09:13:53 +0800

    Update code_challenge9.py

--- code_challenge9.py
+++ code_challenge9.py
@@ -1,27 +1,9 @@
-import time
+print("COUNTDOWN TIMER SIMULATOR")
 
-def countdown_timer():
-    try:
-        minutes = int(input("Enter minutes for the countdown: "))
-        seconds = int(input("Enter seconds for the countdown: "))
-        
-        total_seconds = minutes * 60 + seconds
+start = int(input("Enter the starting number for countdown: "))
 
-        if total_seconds <= 0:
-            print("Please enter a positive duration.")
-            return
+print("\nCountdown started:")
+for i in range(start, 0, -1):
+    print(i)
 
-        while total_seconds > 0:
-            mins, secs = divmod(total_seconds, 60)
-            timer_format = '{:02d}:{:02d}'.format(mins, secs)
-            print(timer_format, end='\r')
-            time.sleep(1)
-            total_seconds -= 1
-
-        print("Countdown finished!")
-
-    except ValueError:
-        print("Invalid input. Please enter integers for minutes and seconds.")
-
-if __name__ == "__main__":
-    countdown_timer()
+print("Liftoff!")
